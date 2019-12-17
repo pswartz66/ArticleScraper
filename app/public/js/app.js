27e63ab04d6c50ff2c06dc3c81d8ae444b539d02
Phil Swartz <pswartz.sju@gmail.com>
2019-12-16 21:20:46 -0500

    removed some unwanted code and add the delete from saved logic

--- app/public/js/app.js
+++ app/public/js/app.js
@@ -5,43 +5,30 @@ $(".scrape-btn").on("click", function(){
     $.ajax({
         method: "GET",
         url: "/scrape"
-
     }).done(function(data){
-
-        console.log(data);
         window.location = "/"
-
     });
-
 });
 
 
 $(".save-article").on("click", function(){
 
     event.preventDefault();
-
     let id = $(this).attr("data-id");
-
-    console.log(id);
 
     $.ajax({
         method: "POST",
         url: "/saved/" + id
     }).done(function(data){
-        console.log(data);
-        window.location.reload();/*  = "/" */
+        window.location.reload();
     });
 
 });
 
 
 $(".clear-articles-non-saved").on("click", function(){
-
     event.preventDefault();
-
     let id = $(this).attr("data-id");
-
-    console.log(id);
 
     $.ajax({
         method: "GET",
@@ -54,28 +41,20 @@ $(".clear-articles-non-saved").on("click", function(){
 });
 
 $(".clear-articles-saved").on("click", function(){
-
     event.preventDefault();
-
     let id = $(this).attr("data-id");
-
-    console.log(id);
-
     $.ajax({
         method: "GET",
         url: "/cleared/saved"
     }).done(function(data){
         console.log(data);
-        window.location.reload();/*  = "/" */
+        window.location.reload();
     });
-
 });
 
 
 // save article note from inside modal
 $(".save-note").on("click", function(){
-
-    
     let id = $(this).attr("data-id");
     let noteText = $(".new-note"+id).val();
 
@@ -87,19 +66,14 @@ $(".save-note").on("click", function(){
             note: noteText
         }
     }).done(function(data){
-
-        // console.log(data);
         window.location = "/saved"
-
     })
-
 });
 
 
 
 // display saved notes inside modal
 $(document).on("click", ".article-notes", function(){
-
 
     $(".note-container").empty();
     $(".note-container").val("");
@@ -110,25 +84,46 @@ $(document).on("click", ".article-notes", function(){
         method: "GET",
         url: "/saved/notes/"+thisId
     })
-    
         .then(function(data){
-
-            console.log(data.notes);
 
             for (let i = 0; i < data.notes.length; i++) {
                 let newLi = $("<li>");
                 newLi.addClass("list-group-item note"+data.notes[i]._id);
                 newLi.text(data.notes[i].note);
-                newLi.append("<button data-id='" + data.notes[i]._id + "' id='delete-note'>X</button>");
+                newLi.append("<button type='button' data-id='" + data.notes[i]._id + "' class='delete-note'>X</button>");
                 $(".note-container").append(newLi);
-            }
-            
+            }  
         })
-    
-        /* $(".new-note").val("");
-        $(".new-note").attr("placeholder", "New Note..."); */
-
 })
+
+// delete note from modal
+$(document).on("click", ".delete-note", function(){
+    let id = $(this).attr("data-id");
+    $.ajax({
+        method: "GET",
+        url: "/delete/notes/" + id
+    }).done(function(data){
+        window.location = "/saved"
+    })
+});
+
+// delete from the saved template
+$(document).on("click", ".delete-saved", function(){
+
+    let id = $(this).attr("data-id");
+    $.ajax({
+        method: "GET",
+        url: "/delete/saved/" + id
+    }).done(function(data){
+        window.location = "/saved"
+    })
+
+});
+
+
+
+
+
 
 
 
--- app/server.js
+++ app/server.js
@@ -48,23 +48,16 @@ mongoose.connect("mongodb://localhost/mongoHeadlines", { useNewUrlParser: true }
 const MONGOOSE_URI = process.env.MONGOOSE_URI || "mongodb://localhost/mongoHeadlines";
 mongoose.connect(MONGOOSE_URI);
 
-
-
 // start the server
 app.listen(PORT, function () {
     console.log("Application running @ http://localhost:" + PORT);
 })
 
-
 // get the web scraped data in scrape route, create db collection
 app.get("/scrape", function (req, res) {
-
     axios.get("https://theundefeated.com/sports/").then(function (response) {
-
         const $ = cheerio.load(response.data);
-
         console.log($);
-
         $("section").each(function (i, ele) {
 
             let result = {};
@@ -74,46 +67,31 @@ app.get("/scrape", function (req, res) {
             result.link = $(this).find(".heading").parent('a').attr('href');
             result.saved = false;
 
-            console.log(result);
             // res.json(result);
-
-
             // create our db collection
             db.Article.create(result).then(function (dbArticle) {
                 res.json(dbArticle);
             }).catch(function (err) {
                 console.log(err);
             });
-
-
         });
-
         res.send("scrape complete");
-
     }).catch(function (err) {
         console.log(err);
     });
-
-
 });
 
 
 // get all non-saved articles from db here
 app.get("/", function (req, res) {
-
     db.Article.find({saved: false}, function (err, dbData) {
-
         const hbsObject = {
             articles: dbData
         };
-
         // load index.handlebars file at root window
         res.render("home", hbsObject);
-
     });
-
 });
-
 
 
 // get the saved route and load handlebars saved template
@@ -147,7 +125,6 @@ app.post("/saved/:id", function (req, res) {
     });
 });
 
-
 // clear non-saved articles from db and send back to home template
 app.get("/cleared/non-saved", function (req, res) {
     db.Article.remove({saved: false}, function (err, updatedDB) {
@@ -156,7 +133,6 @@ app.get("/cleared/non-saved", function (req, res) {
         } else {
             const hbsObject = {
                 articles: updatedDB
-
             };
             // load saved.handlebars file at saved path
             res.render("saved", hbsObject);
@@ -180,45 +156,54 @@ app.get("/cleared/saved", function (req, res) {
 });
 
 
+// get the saved notes and populate them in the Article db
 app.get("/saved/notes/:id", function(req, res) {
-
     db.Article.findOne({_id: req.params.id})
-
     .populate("notes")
-
     .then(function(notes){
-
         console.log(notes);
-
         res.send(notes);
-
     }).catch(function(err){
         if (err) {
             console.log(err);
         }
     })
-
 })
-
 
 // post the saved note in Note db, populate notes inside of Article db above
 app.post("/saved/notes/:id", function(req, res){
-
-    console.log('article id:' +req.params.id);
     db.Note.create(req.body).then(function(dbNote){
-
         // find id inside of Article collection and push the associated notes into the Article
         return db.Article.findOneAndUpdate({_id: req.params.id}, {$push: {notes: dbNote._id}}, {new: true});
-        
-
     }).then(function(dbArticle){
-
         res.json(dbArticle);
-
     }).catch(function(err){
         if(err) {
             res.json(err);
         }
     });
-
 })
+
+// delete a specific note in the modal
+app.get("/delete/notes/:id", function(req, res){
+    db.Note.remove({_id: req.params.id}).then(function(response){
+        res.json(response);
+    })
+});
+
+
+// delete a specific note in the modal
+app.get("/delete/saved/:id", function(req, res){
+    db.Article.remove({_id: req.params.id}, function(err, updatedDB){
+
+        if(err){
+            res.json(err);
+        } else {
+            const hbsObject = {
+                articles: updatedDB
+            };
+            // load saved.handlebars file at saved path
+            res.render("saved", hbsObject);
+        }
+    })
+});
